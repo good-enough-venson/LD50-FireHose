f5e1271ac6058faadad0be994766e048b1b06415
good-enough-venson <ephraim.goodenough@gmail.com>
2022-04-02 10:14:43 -0500

    stuff.. I may need to update gitignore...

--- Assets/Scripts/ApartmentManager.cs
+++ Assets/Scripts/ApartmentManager.cs
@@ -53,16 +53,4 @@ public class ApartmentManager : MonoBehaviour
             }
         }
     }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
