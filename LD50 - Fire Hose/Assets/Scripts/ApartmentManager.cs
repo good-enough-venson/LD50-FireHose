2011f9ee8e57a94841d9b2302c9d3f842c2e5a7c
Ephraim Goodenough <ephraim.goodenough@neuromatics.net>
2022-04-02 08:06:23 -0500

    Moved Existing Unity Project Over

---
+++ LD50 - Fire Hose/Assets/Scripts/ApartmentManager.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApartmentManager : MonoBehaviour
+{
+    public GameObject roomPrefab;
+    public GameObject resiPrefab;
+    public int columns = 4;
+    public int rows = 5;
+    public Vector3 roomSize = new Vector3(6f, 3.8f, 4f);
+    public Room[] roomDatae = { };
+
+    public enum FireState {
+        None, Smoke, Fire, Burnt
+    }
+
+    [System.Serializable]
+    public struct Room
+    {
+        public GameObject roomObj;
+        public Vector2Int roomNum;
+        public FireState fireState;
+        public float lastFireStateChange;
+        public List<GameObject> residents;
+    }
+
+    private void Awake()
+    {
+        roomDatae = new Room[columns * rows];
+        for (int y = 0; y < rows; y++)
+        {
+            for (int x = 0; x < columns; x++)
+            {
+                Vector3 p = new Vector3(
+                    transform.position.x + roomSize.x * x,
+                    transform.position.y + roomSize.y * y,
+                    transform.position.z
+                );
+
+                var r = Instantiate<GameObject> (
+                    roomPrefab, p, Quaternion.identity, transform
+                );
+
+                roomDatae[y * columns + x] = new Room
+                {
+                    roomObj = r,
+                    roomNum = new Vector2Int(x, y),
+                    fireState = FireState.None,
+                    lastFireStateChange = 0,
+                    residents = new List<GameObject>()
+                };
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
